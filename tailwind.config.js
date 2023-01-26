6d9423a1a0e3fe880d6d970889fe4fcdaeb9b654
tobias57 <tobias@sthorm.io>
2023-01-26 19:27:07 -0300

    feat: configuration of tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+const globalTheme = require("./src/lib/design/theme");
+const textStyles = require("./src/lib/design/typography");
+
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    ...textStyles.typographies,
+    extend: {
+      ...globalTheme,
+    },
+  },
+  plugins: [plugin(textStyles.register), require("@tailwindcss/line-clamp")],
+};
